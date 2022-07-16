37d29034309d55d899aba8e94c20d208d9d3437d
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-07-16 13:04:41 +0200

    Remove unnecessary test case

--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/dataflow/TypeDeclTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/dataflow/TypeDeclTests.scala
@@ -47,27 +47,4 @@ class TypeDeclTests extends KotlinCode2CpgFixture(withOssDataflow = true) {
         Set(List(("f1(p)", Some(3)), ("aClass.x = p", Some(5)), ("println(aClass.x)", Some(6))))
     }
   }
-
-  "CPG for code with member assignment" should {
-    val cpg = code("""
-      |class AClass {
-      |    var x: String = "INITIAL"
-      |}
-      |
-      |fun doSomething(p1: String): String {
-      |    val aClass = AClass()
-      |    aClass.x = p1
-      |    val aVal = aClass.x
-      |    return "NOTHING"
-      |}
-      |""".stripMargin)
-
-    "should find a flow through the assignment" in {
-      val source = cpg.method.name("doSomething").parameter
-      val sink   = cpg.identifier.code("aVal")
-      val flows  = sink.reachableByFlows(source)
-      flows.map(flowToResultPairs).toSet shouldBe
-        Set(List(("doSomething(p1)", Some(6)), ("aClass.x = p1", Some(8)), ("val aVal = aClass.x", Some(9))))
-    }
-  }
 }
