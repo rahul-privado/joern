ed18cc756989ef93baaad2afee85bfd695475fed
rahul-privado <rahul.godbole@privado.ai>
2023-06-02 12:45:32 +0530

    Corrected test

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/dataflow/DataFlowTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/dataflow/DataFlowTests.scala
@@ -8,38 +8,25 @@ class DataFlowTests extends DataFlowCodeToCpgSuite {
 
   "CPG for code with flow through a function and if-elseif-else" should {
     val cpg = code("""
-        |def add_three_numbers(num1, num2, num3)
-        | sum = num1 + num2 + num3
-        | p = sum
-        | q = p
-        | r = q
-        | s = r
-        | x = 7
+        |x = 2
+        |a = x
+        |b = 0
         |
-        |if x > 4
-        | a = x + 1
-        | return a
-        |elseif x < 2
-        | b = x - 1
-        | return b
+        |if a > 2
+        |    b = a + 3
+        |elseif a > 4
+        |    b = a + 5
+        |elseif a > 8
+        |    b = a + 5
         |else
-        | c = x + 10
-        | return c
+        |    b = a + 9
         |end
         |
-        |end
-        |
-        |n1 = 1
-        |n2 = 2
-        |n3 = 3
-        |
-        |n = add_three_numbers( n1, n2, n3 )
-        |to_print = n
-        |puts to_print
+        |puts(b)
         |""".stripMargin)
 
     "find flows to the sink" in {
-      val source = cpg.identifier.name("n1").l
+      val source = cpg.identifier.name("x").l
       val sink   = cpg.call.name("puts").l
       sink.reachableByFlows(source).l.size shouldBe 2
     }
